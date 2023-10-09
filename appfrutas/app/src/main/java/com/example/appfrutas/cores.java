4188e0bd34536bb7c79b41d365b188d8b38d183d
anacarolinavoto <92889219+anacarolinavoto@users.noreply.github.com>
2023-10-09 10:01:56 -0300

    19.10.23
    Página de cores e página de frutas (layout)

---
+++ appfrutas/app/src/main/java/com/example/appfrutas/cores.java
@@ -0,0 +1,14 @@
+package com.example.appfrutas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class cores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cores);
+    }
+}
